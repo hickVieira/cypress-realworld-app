502342d33b8ec939643fab1c6db9f3485c70f43e
hickVieira <lhlvieirag@gmail.com>
2024-06-20 13:14:49 -0300

    added utils functions for easy page control

--- cypress/tests/my-tests/auth.spec.ts
+++ cypress/tests/my-tests/auth.spec.ts
@@ -1,114 +1,61 @@
+import { Pages, utils } from "./utils"
+
 describe("Login and Register tests", function () {
     beforeEach(function () {
         cy.task("db:seed")
     })
-    
+
     it("should login successfully", function () {
-        cy.visit("/signin")
-        cy.get("[data-test='signin-username']").type("Arvilla_Hegmann")
-        cy.get("[data-test='signin-password']").type("s3cret")
-        cy.get("[data-test='signin-submit']").click()
-        cy.location("pathname").should("equal", "/")
+        utils.signIn("Arvilla_Hegmann", "s3cret", Pages.Root)
     })
 
     it("should display error message when invalid username", function () {
-        cy.visit("/signin")
-        cy.get("[data-test='signin-username']").type("sdasdsadsad")
-        cy.get("[data-test='signin-password']").type("s3cret")
-        cy.get("[data-test='signin-submit']").click()
-        cy.location("pathname").should("equal", "/signin")
+        utils.signIn("invalidUserName", "s3cret", Pages.SignIn)
         cy.contains("Username or password is invalid").should("be.visible")
     })
 
     it("should display error message when invalid password", function () {
-        cy.visit("/signin")
-        cy.get("[data-test='signin-username']").type("Arvilla_Hegmann")
-        cy.get("[data-test='signin-password']").type("12345")
-        cy.get("[data-test='signin-submit']").click()
-        cy.location("pathname").should("equal", "/signin")
+        utils.signIn("Arvilla_Hegmann", "invalidPassword", Pages.SignIn)
         cy.contains("Username or password is invalid").should("be.visible")
     })
 
     it("should register successfully", function () {
-        cy.visit("/signup")
-        cy.get("[data-test='signup-first-name']").type("Fulano")
-        cy.get("[data-test='signup-last-name']").type("Ciclano")
-        cy.get("[data-test='signup-username']").type("TestUser")
-        cy.get("[data-test='signup-password']").type("s3cret")
-        cy.get("[data-test='signup-confirmPassword']").type("s3cret")
-        cy.get("[data-test='signup-submit']").click()
-        cy.location("pathname").should("equal", "/signin")
+        utils.signUp("Fulano", "Ciclano", "TestUser", "s3cret", "s3cret", Pages.SignIn)
     })
 
     it("should display error message when missing first name", function () {
-        cy.visit("/signup")
-        cy.get("[data-test='signup-first-name']").type('{selectall}{backspace}')
-        cy.get("[data-test='signup-last-name']").type("Ciclano")
-        cy.get("[data-test='signup-username']").type("TestUser")
-        cy.get("[data-test='signup-password']").type("s3cret")
-        cy.get("[data-test='signup-confirmPassword']").type("s3cret")
-        cy.contains("First Name is required").should("be.visible")
-        cy.get("[data-test='signup-submit']").should("be.disabled")
-        cy.location("pathname").should("equal", "/signup")
+        utils.signUp("{selectall}{backspace}", "Ciclano", "TestUser", "s3cret", "s3cret", Pages.SignUp)
+        cy.contains("First Name is required").should("be.visible") // error message
+        cy.get("[data-test='signup-submit']").should("be.disabled") // locked submit button
     })
 
     it("should display error message when missing last name", function () {
-        cy.visit("/signup")
-        cy.get("[data-test='signup-first-name']").type("Fulano")
-        cy.get("[data-test='signup-last-name']").type('{selectall}{backspace}')
-        cy.get("[data-test='signup-username']").type("TestUser")
-        cy.get("[data-test='signup-password']").type("s3cret")
-        cy.get("[data-test='signup-confirmPassword']").type("s3cret")
-        cy.contains("Last Name is required").should("be.visible")
-        cy.get("[data-test='signup-submit']").should("be.disabled")
-        cy.location("pathname").should("equal", "/signup")
+        utils.signUp("Fulano", "{selectall}{backspace}", "TestUser", "s3cret", "s3cret", Pages.SignUp)
+        cy.contains("Last Name is required").should("be.visible") // error message
+        cy.get("[data-test='signup-submit']").should("be.disabled") // locked submit button
     })
 
     it("should display error message when missing username", function () {
-        cy.visit("/signup")
-        cy.get("[data-test='signup-first-name']").type("Fulano")
-        cy.get("[data-test='signup-last-name']").type("Ciclano")
-        cy.get("[data-test='signup-username']").type('{selectall}{backspace}')
-        cy.get("[data-test='signup-password']").type("s3cret")
-        cy.get("[data-test='signup-confirmPassword']").type("s3cret")
-        cy.contains("Username is required").should("be.visible")
-        cy.get("[data-test='signup-submit']").should("be.disabled")
-        cy.location("pathname").should("equal", "/signup")
+        utils.signUp("Fulano", "Ciclano", "{selectall}{backspace}", "s3cret", "s3cret", Pages.SignUp)
+        cy.contains("Username is required").should("be.visible") // error message
+        cy.get("[data-test='signup-submit']").should("be.disabled") // locked submit button
     })
 
     it("should display error message when missing password", function () {
-        cy.visit("/signup")
-        cy.get("[data-test='signup-first-name']").type("Fulano")
-        cy.get("[data-test='signup-last-name']").type("Ciclano")
-        cy.get("[data-test='signup-username']").type('TestUser')
-        cy.get("[data-test='signup-password']").type('{selectall}{backspace}')
-        cy.get("[data-test='signup-confirmPassword']").type("s3cret")
-        cy.contains("Enter your password").should("be.visible")
-        cy.get("[data-test='signup-submit']").should("be.disabled")
-        cy.location("pathname").should("equal", "/signup")
+        utils.signUp("Fulano", "Ciclano", "TestUser", "{selectall}{backspace}", "s3cret", Pages.SignUp)
+        cy.contains("Enter your password").should("be.visible") // error message
+        cy.get("[data-test='signup-submit']").should("be.disabled") // locked submit button
     })
 
     it("should display error message when passwords not matching", function () {
-        cy.visit("/signup")
-        cy.get("[data-test='signup-first-name']").type("Fulano")
-        cy.get("[data-test='signup-last-name']").type("Ciclano")
-        cy.get("[data-test='signup-username']").type("TestUser")
-        cy.get("[data-test='signup-password']").type("12345")
-        cy.get("[data-test='signup-confirmPassword']").type("54321")
-        cy.contains("Password does not match").should("be.visible")
-        cy.get("[data-test='signup-submit']").should("be.disabled")
-        cy.location("pathname").should("equal", "/signup")
+        utils.signUp("Fulano", "Ciclano", "TestUser", "s3cret", "DIFFERENT PASSWORD", Pages.SignUp)
+        cy.contains("Password does not match").should("be.visible") // error message
+        cy.get("[data-test='signup-submit']").should("be.disabled") // locked submit button
     })
 
     it("should display error message when password having wrong length", function () {
-        cy.visit("/signup")
-        cy.get("[data-test='signup-first-name']").type("Fulano")
-        cy.get("[data-test='signup-last-name']").type("Ciclano")
-        cy.get("[data-test='signup-username']").type("TestUser")
-        cy.get("[data-test='signup-password']").type("123")
-        cy.get("[data-test='signup-confirmPassword']").type("123")
-        cy.contains("Password must contain at least 4 characters").should("be.visible")
-        cy.get("[data-test='signup-submit']").should("be.disabled")
-        cy.location("pathname").should("equal", "/signup")
+        utils.signUp("Fulano", "Ciclano", "TestUser", "123", "123", Pages.SignUp)
+        cy.contains("Password must contain at least 4 characters").should("be.visible") // error message
+        cy.get("[data-test='signup-submit']").should("be.disabled") // locked submit button
     })
 })
---
+++ cypress/tests/my-tests/utils.ts
@@ -0,0 +1,41 @@
+export enum Pages {
+    Root = "/",
+    SignIn = "/signin",
+    SignUp = "/signup",
+}
+
+export class utils {
+    public static goToPage(page: Pages) {
+        cy.visit(page)
+    }
+
+    public static locationShouldBe(page: Pages) {
+        cy.location("pathname").should("equal", page)
+    }
+
+    public static signUp(firstName: string, lastName: string, username: string, password: string, passwordConfirm: string, expectedPage?: Pages) {
+        utils.goToPage(Pages.SignUp)
+        cy.get("[data-test='signup-first-name']").type(firstName)
+        cy.get("[data-test='signup-last-name']").type(lastName)
+        cy.get("[data-test='signup-username']").type(username)
+        cy.get("[data-test='signup-password']").type(password)
+        cy.get("[data-test='signup-confirmPassword']").type(passwordConfirm)
+        cy.get("[data-test='signup-submit']").then(($button) => {
+            if(!$button.is(":disabled"))
+                $button.click()
+        })
+
+        if (expectedPage)
+            utils.locationShouldBe(expectedPage)
+    }
+
+    public static signIn(username: string, password: string, expectedPage?: Pages) {
+        utils.goToPage(Pages.SignIn)
+        cy.get("[data-test='signin-username']").type(username)
+        cy.get("[data-test='signin-password']").type(password)
+        cy.get("[data-test='signin-submit']").click()
+
+        if (expectedPage)
+            utils.locationShouldBe(expectedPage)
+    }
+}
